683ebcf73c15237e15d551d0b9d09ed02a3c19ff
Chetankumar Wakude <c.wakude@mobily.com.sa>
2020-02-06 22:31:24 +0530

    First commit

--- OTPCustomView/ViewController.swift
+++ OTPCustomView/ViewController.swift
@@ -6,15 +6,62 @@
 //  Copyright © 2020 Chetan. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var otpView: OTPView!
+    
+    var enteredOtp: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        otpView.otpFieldsCount = 4
+        otpView.otpFieldDefaultBorderColor = UIColor.blue
+        otpView.otpFieldEnteredBorderColor = UIColor.green
+        otpView.otpFieldErrorBorderColor = UIColor.red
+        otpView.otpFieldBorderWidth = 2
+        otpView.delegate = self
+        otpView.shouldAllowIntermediateEditing = false
+        
+        // Create the UI
+        otpView.initializeUI()
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
 
 
+extension ViewController: OTPViewDelegate {
+    func hasEnteredAllOTP(hasEntered: Bool) -> Bool {
+        print("Has entered all OTP? \(hasEntered)")
+        
+        return true
+    }
+    
+    func shouldBecomeFirstResponderForOTP(otpFieldIndex index: Int) -> Bool {
+        return true
+    }
+    
+    func enteredOTP(otpString: String) {
+        enteredOtp = otpString
+        print("OTPString: \(otpString)")
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let replacedText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
+        
+        return true
+    }
 }
 
